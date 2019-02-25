a8e39ce1569d1a175414455b087da120d9889f86
AkhilYerraRutgers <ay260@scarletmail.rutgers.edu>
2019-02-24 22:27:00 -0500

    file changes

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ const Spotify = require('node-spotify-api')
 const app = express();
 const port=process.env.PORT || 4200
 
-const client = new Twitter({
-  consumer_key: 'cqpW07aEmhbpeGAI0Jf6eT4hC',
-  consumer_secret: 'luPksK1GDz3KX2GDcAUGJ8oVzZUjT5otlqX2SlE9yFYLaooH1v',
-  access_token: '491873327-DcdcK23zGnW0NNiaJw6DWczSnTa48iCz3soOoYUK',
-  access_token_secret: 'B0b2PnzOhwlRUJ1efvnZHftFBs6y24c8ihaDbiKqBdFsM'
-});
-
-var spotify = new Spotify({
-  id: '977951575bfb4d16a577669f4ca18488',
-  secret: '2ccfabf3aa804373aa6db9f2e962e9ca'
-});
 
 app.use(require('cors')());
 app.use(require('body-parser').json());
