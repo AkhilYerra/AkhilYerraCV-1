bd6a5179708d3fc73b375f0be939409fb573352c
AkhilYerraRutgers <ay260@scarletmail.rutgers.edu>
2019-02-24 22:11:06 -0500

    idk changes

--- src/app/model/spotifyAPI.service.ts
+++ src/app/model/spotifyAPI.service.ts
@@ -15,21 +15,21 @@ export class SpotifyService {
 
   searchArtist(id: string){
     console.log(`${environment.api}/artists${id}`)
-     return this.http.get<Artist>(`${environment.api}/artists${id}`)
+     return this.http.get<Artist>(`/api/artists${id}`)
   }
   searchAlbum(id : string){
     console.log(`${environment.api}/albums${id}`)
-    return this.http.get<Album>(`${environment.api}/albums${id}`)
+    return this.http.get<Album>(`/api/albums${id}`)
   }
 
   searchArtistByName(id: string){
     console.log(`${environment.api}/searchArtist${id}`)
-     return this.http.get<searchArtist>(`${environment.api}/searchArtist${id}`)
+     return this.http.get<searchArtist>(`/api/searchArtist${id}`)
   }
 
   searchSongByName(id: string){
     console.log(`${environment.api}/searchSong${id}`)
-     return this.http.get<searchTrack>(`${environment.api}/searchSong${id}`)
+     return this.http.get<searchTrack>(`/api/searchSong${id}`)
   }
 
   searchAlbumByName(id: string){
